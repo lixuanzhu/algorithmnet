3d8706f902c84f3da8c43064015885b713b4df4c
Sean Zhu <seanzhu@microsoft.com>
2018-12-16 23:25:14 -0800

    298 fix

--- algorithm/LC298-BinaryTreeLongestConsecutiveSequence.cs
+++ algorithm/LC298-BinaryTreeLongestConsecutiveSequence.cs
@@ -10,30 +10,28 @@ namespace algorithm
 
         public void Run()
         {
-            TreeNode root = new TreeNode(1);
+            TreeNode root = new TreeNode(5);
             root.left = new TreeNode(2);
-            root.right = new TreeNode(4);
-            root.left.right = new TreeNode(4);
-            root.left.left = new TreeNode(7);
-            root.left.right.right = new TreeNode(5);
+            root.left.right = new TreeNode(3);
+            root.left.right.right = new TreeNode(4);
             root.left.right.right.left = new TreeNode(6);
+            root.right = new TreeNode(6);
+            root.left.left = new TreeNode(7);
+            root.left.right.right.right = new TreeNode(9);
             Test.Verify(3, longestConsecutive(root));
         }
         public int longestConsecutive(TreeNode root)
         {
-            longestConsecutive(root, root.val);
-            return res;
+            return longestConsecutive(root, root.val - 1, 0);
         }
 
-        public int longestConsecutive(TreeNode root, int rootval)
+        public int longestConsecutive(TreeNode root, int pre, int count)
         {
-            if (root == null) return 0;
-            int left = longestConsecutive(root.left, root.val + 1);
-            int right = longestConsecutive(root.right, root.val + 1);
-            int max = Math.Max(left, right) + 1;
-            res = Math.Max(res, max);
-            if (rootval != root.val) max = 0;
-            return max;
+            if (root == null) return count;
+            count = root.val == pre + 1 ? count + 1 : 1;
+            int max = Math.Max(longestConsecutive(root.left, root.val, count), longestConsecutive(root.right, root.val, count));
+            return Math.Max(max, count);
+            
         }
     }
 }
